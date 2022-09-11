d0133f00fe58dbc11352dcee8918ad6f18395ca5
Chioma <anigboguchioma31@gmail.com>
2022-09-11 04:57:35 +0100

    edited my db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
-import mongoose from 'mongoose' 
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const MONGODB_URI = process.env.MONGODB_URI
 
 const connectDB = async () =>{
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      
-    })
+    const conn = await mongoose.connect(MONGODB_URI)
     console.log(`Mongodb Connected: ${conn.connection.host}`)
   } catch (error) {
     console.error(`Error: ${error.message}`)
