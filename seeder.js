9ee411779e27eea0e46c3b948450961c84d16cbb
Chioma <anigboguchioma31@gmail.com>
2022-09-14 12:18:15 +0100

    edited senders email

--- seeder.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from 'dotenv';
-import admin from './config/admin.js'
-import connectDB from './config/connectDB.js'
-
-
-connectDB()
-dotenv.config()
-
